11c144f4638666cda9ae6ffdf1a96016f5f909c0
Sebastian Sagan <sebsag002@utp.edu.pl>
2018-10-03 22:02:07 +0200

    matma pc

---
+++ matma/zad2.js
@@ -0,0 +1,13 @@
+let a = 0;
+let a1 = 1;
+let a2 = 2;
+let sum = 2;
+for (a; a < 4000000; a + a){
+    a = a1 + a2;
+    a1 = a2;
+    a2 = a;
+    if (a % 2 === 0) {
+        sum +=a;
+    }
+}
+console.log(sum);
