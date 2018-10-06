68e2232349662cf5ca79d9ce0ce920d644bfc81c
Sebastian Sagan <sebsag002@utp.edu.pl>
2018-10-07 00:06:49 +0200

    Update matma/zad10.js

--- matma/zad10.js
+++ matma/zad10.js
@@ -6,23 +6,16 @@ for (let i = 0; i < max; i += 1) {
 }
 
 for (let j = 2; j < Math.sqrt(max); j += 1) {
-    if (lista[j] !== 0) {
-        for (let k = j * 2; k < max; k += j) {
-            lista[k] = 0;
-          }
+  if (lista[j] !== 0) {
+    for (let k = j * 2; k < max; k += j) {
+      lista[k] = 0;
     }
-  
+  }
 }
 
 for (let c = 2; c < max; c += 1) {
   if (lista[c] !== 0) {
     sum += lista[c];
-    //console.log(lista[c]);
-    //console.log(sum);
   }
 }
-console.log(lista);
 console.log(sum);
-
-// poprawny 142913828922
-//          142913828939
