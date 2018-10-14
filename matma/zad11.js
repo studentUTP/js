54b1251b8342065face98b45dfb5d075c0b6356c
Sebastian Sagan <sebsag002@utp.edu.pl>
2018-10-14 20:36:40 +0200

    matma pc

--- matma/zad11.js
+++ matma/zad11.js
@@ -2,19 +2,21 @@ const fs = require("fs");
 
 let lista = [];
 const pol = [];
-let licznik = 0;
-let licznik2 = 0;
+
+
 let max = 0;
 
 fs.readFile(process.argv[2], "utf8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
+    let licznik = 0;
     for (let i = 0; i < data.length; i += 1) {
       lista[licznik] = data[i];
       licznik += 1;
     }
     lista = lista.filter(str => /\S/.test(str));
+    let licznik2 = 0;
     for (let j = 0; j < lista.length; j += 2) {
       if (licznik2 > 0) {
         pol[licznik2] = Number(lista[j] * 10) + Number(lista[j + 1]);
@@ -24,7 +26,6 @@ fs.readFile(process.argv[2], "utf8", (err, data) => {
         licznik2 += 1;
       }
     }
-    console.log(pol);
     for (let k = 0; k < pol.length; k += 1) {
       const makspoziom = pol[k] * pol[k + 1] * pol[k + 2] * pol[k + 3];
       const makspion = pol[k] * pol[k + 20] * pol[k + 40] * pol[k + 60];
@@ -32,23 +33,19 @@ fs.readFile(process.argv[2], "utf8", (err, data) => {
       const maksskosl = pol[k] * pol[k + 19] * pol[k + 38] * pol[k + 57];
       if (makspoziom > max) {
         max = makspoziom;
-        console.log(max);
-        console.log(`poziom ${pol[k]}`, pol[k + 1], pol[k + 2], pol[k + 3]);
+        console.log(`poziom Iloczyn: ${max} ${pol[k]}`, pol[k + 1], pol[k + 2], pol[k + 3]);
       }
       if (makspion > max) {
         max = makspion;
-        console.log(max);
-        console.log(`pion ${pol[k]}`, pol[k + 20], pol[k + 40], pol[k + 60]);
+        console.log(`pion Iloczyn: ${max} ${pol[k]}`, pol[k + 20], pol[k + 40], pol[k + 60]);
       }
       if (maksskosp > max) {
         max = maksskosp;
-        console.log(max);
-        console.log(`skosp ${pol[k]}`, pol[k + 21], pol[k + 42], pol[k + 63]);
+        console.log(`skosP Iloczyn: ${max} ${pol[k]}`, pol[k + 21], pol[k + 42], pol[k + 63]);
       }
       if (maksskosl > max) {
         max = maksskosl;
-        console.log(max);
-        console.log(`skosl ${pol[k]}`, pol[k + 21], pol[k + 42], pol[k + 63]);
+        console.log(`skosL Iloczyn: ${max} ${pol[k]}`, pol[k + 19], pol[k + 38], pol[k + 57]);
       }
     }
   }
